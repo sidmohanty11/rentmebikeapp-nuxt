537de7a5ab8e04881e3fc0dc8ca4a29f8e4488ba
sidmohanty11 <sidmohanty11@gmail.com>
2021-08-24 00:34:54 +0530

    dynamically rendering bikes for each id

--- pages/bike/_id.vue
+++ pages/bike/_id.vue
@@ -1,35 +1,32 @@
 <template>
-  <div class="container">
-    <img height="400px" src="~/assets/img/b19.jpg" alt="" />
+  <div v-if="bike" class="container">
+    <img
+      height="400px"
+      :src="require(`@/assets/img/${bike.image || 'b1.jpg'}`)"
+      alt=""
+    />
     <div class="container-para">
-      <h1>Duke RC 200</h1>
+      <h1>{{ bike.title }}</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ipsum
-        voluptas nisi ab facere eum reiciendis, aspernatur quos, nobis explicabo
-        officiis sed dolorum iste doloribus sapiente atque labore suscipit odit.
-        Quo officia eos accusantium. Eius aspernatur corporis earum nulla
-        voluptas quasi corrupti modi asperiores accusantium similique blanditiis
-        officia, quaerat, optio delectus fuga neque minus eligendi laboriosam
-        esse dolor enim facere! Esse, repellat accusamus. Mollitia, sapiente
-        vero? Sed, in, ex sapiente consequatur et, pariatur excepturi provident
-        aperiam inventore fugit adipisci numquam vel vero illum minus vitae
-        magnam. Ad quae voluptates aspernatur? Modi quos ipsam veritatis velit
-        cumque optio, quae beatae veniam doloremque nobis temporibus quod
-        doloribus sunt, natus in accusantium.
+        {{ bike.snippet }}
       </p>
-      <p class="price-tag">Price - Rs. 2000/day</p>
+      <p>
+        {{ bike.description }}
+      </p>
+      <p class="price-tag">Price - Rs. {{ bike.price }}/day</p>
       <a-button ghost> Rent me this! </a-button>
     </div>
   </div>
+  <div v-else class="pagenotfound">Page Not Found</div>
 </template>
 
 <script>
 export default {
-  // computed: {
-  //   bike() {
-  //     return
-  //   }
-  // }
+  computed: {
+    bike() {
+      return this.$store.getters.getBikeById(this.$route.params.id);
+    }
+  }
 };
 </script>
 
@@ -50,6 +47,10 @@ export default {
 }
 
 .price-tag {
+  color: aqua;
+}
+
+.pagenotfound {
   color: aqua;
 }
 </style>
